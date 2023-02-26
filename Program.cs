9f60bb418fe2c1603fdc20fbe05e7ac3a834023e
Andre Felipe <andrefelipe.pi2@gmail.com>
2023-02-25 21:20:07 -0300

    Estrutura sequencial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Diferenca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite quatro valores: ");
+            int a = int.Parse (Console.ReadLine());
+            int b = int.Parse (Console.ReadLine());
+            int c = int.Parse (Console.ReadLine());
+            int d = int.Parse (Console.ReadLine());
+
+            int diferenca = a * b - c * d;
+
+            Console.WriteLine("Diferenca = " + diferenca);
+        }
+    }
+}
